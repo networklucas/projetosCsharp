eaa219c0b13ff65d59adad52d8dd7cb3d314f7db
networklucas <networklucas@hotmail.com>
2020-03-20 14:42:21 -0300

    hello teste comit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace projetosCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("testeconexão git");
+            Console.WriteLine();
+        }
+    }
+}
