7703251f584281c6ec756eb025e199526866cab0
networklucas <networklucas@hotmail.com>
2020-03-20 16:15:45 -0300

    inserção de variaveis inteiras

--- Program.cs
+++ Program.cs
@@ -6,12 +6,16 @@ namespace projetosCsharp
     {
         static void Main(string[] args)
         {
+            int idade;
+            idade = 24;
             Console.WriteLine("Hello World!");
             Console.WriteLine("testeconexão git");
             Console.WriteLine();
             //teste comitado
             Console.WriteLine("Mais um Teste mudança 15:38");
             Console.Write("mais um teste comit");
+            Console.WriteLine("Ideade é igual "+idade);
+            
             //marge teste
 
         }
